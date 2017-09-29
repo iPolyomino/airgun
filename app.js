e7f75e00196d52b2feb415bdcc466ebdbd7546d0
iPolyomino <dpm26508@kwansei.ac.jp>
2017-09-29 10:15:06 +0900

    websocketで角度を送るとその角度になるように変更

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const five = require('johnny-five');
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({port: 5500});
-
-wss.on('connection', (ws) => {
-    ws.on('open', () => {
-        ws.send('connected');
-    });
-    ws.on('close', () => {
-    });
-    ws.on('message', (stateID) => {
-        nowLED = stateID;
-        ws.send(`The StateID is ${nowLED}`);
-    });
-});
-
 const board = new five.Board();
 
 board.on('ready', () => {
-    
-
+    const servo = new five.Servo({
+      pin: 10,
+      startAt: 0
+    });
+    wss.on('connection', (ws) => {
+        ws.on('open', () => {
+            ws.send('connected! I will Home Position.');
+            servo.home();
+        });
+        ws.on('close', () => {
+        });
+        ws.on('message', (degrees) => {
+            servo.to(parseInt(degrees));
+            ws.send(`I will turn ${degrees} digrees.`);
+        });
+    });
 });
